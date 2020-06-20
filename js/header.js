f6b92944b2421c44589fb90abc05810f70dd88fe
Hotdoy <rouleau.vincent@gmail.com>
2020-06-20 12:59:49 -0400

    Header interactions

--- js/header.js
+++ js/header.js
@@ -5,13 +5,13 @@ const lastOffset = 0;
 
 for (let onTrigger of onTriggers) {
 	onTrigger.addEventListener("click", function(){
-		b.classList.add("header-panel--on");
+		b.classList.add("header--deployed");
 	})
 }
 
 for (let offTrigger of offTriggers) {
 	offTrigger.addEventListener("click", function(){
-		b.classList.remove("header-panel--on");
+		b.classList.remove("header--deployed");
 	})
 }
 
@@ -19,10 +19,10 @@ window.addEventListener("scroll", function(){
 
    	let currentOffset = window.pageYOffset || document.documentElement.scrollTop;
 
-   	if (currentOffset < lastOffset && window.pageYOffset > 200){
-		b.classList.add("header-onscroll--on"); 
-   	} else if (currentOffset > (lastOffset + 10) || window.pageYOffset < 200) {
-    	b.classList.remove("header-onscroll--on");
+   	if (window.pageYOffset > 200){
+		b.classList.add("header--scrolled"); 
+   	} else if (window.pageYOffset < 200) {
+    	b.classList.remove("header--scrolled");
    	}
 
    	lastOffset = currentOffset;
