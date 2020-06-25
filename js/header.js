7bfc02f67e5485cbe9f4c81d65484fe91c3754ad
Hotdoy <rouleau.vincent@gmail.com>
2020-06-25 13:32:35 -0400

    header as anonymous function

--- js/header.js
+++ js/header.js
@@ -1,29 +1,34 @@
-const b = document.body;
-const onTriggers = document.getElementsByClassName("header__toggle-on");
-const offTriggers = document.getElementsByClassName("header__toggle-off");
+(function () {
+	
+	'use strict';
 
-for (let onTrigger of onTriggers) {
-	onTrigger.addEventListener("click", function(){
-		b.classList.add("header--deployed");
-	})
-}
+	const InitHeader = function() {
+		const b = document.body;
+		const onTriggers = document.getElementsByClassName("header__toggle-on");
+		const offTriggers = document.getElementsByClassName("header__toggle-off");
 
-for (let offTrigger of offTriggers) {
-	offTrigger.addEventListener("click", function(){
-		b.classList.remove("header--deployed");
-	})
-}
+		for (let onTrigger of onTriggers) {
+			onTrigger.addEventListener("click", function(){
+				b.classList.add("header--deployed");
+			})
+		}
 
-window.addEventListener("scroll", function(){
+		for (let offTrigger of offTriggers) {
+			offTrigger.addEventListener("click", function(){
+				b.classList.remove("header--deployed");
+			})
+		}
 
-   	let currentOffset = window.pageYOffset || document.documentElement.scrollTop;
+		window.addEventListener("scroll", function(){
+		   	let currentOffset = window.pageYOffset || document.documentElement.scrollTop;
+		   	if (window.pageYOffset > 200){
+				b.classList.add("header--scrolled"); 
+		   	} else if (window.pageYOffset < 200) {
+		    	b.classList.remove("header--scrolled");
+		   	}
+		}, false);
+	}
 
-   	if (window.pageYOffset > 200){
-		b.classList.add("header--scrolled"); 
-   	} else if (window.pageYOffset < 200) {
-    	b.classList.remove("header--scrolled");
-   	}
+	InitHeader();
 
-}, false);
-
-
+})();
